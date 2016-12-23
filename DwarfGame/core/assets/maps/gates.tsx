874f61be708a9193ea9e24800c875f3f3c6da13e
Calvin <calvin.krist@yahoo.com>
2016-12-22 22:39:29 -0500

    V 0.2.0

--- DwarfGame/core/assets/maps/gates.tsx
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Gates" tilewidth="32" tileheight="32" tilecount="1" columns="0">
- <tile id="1">
-  <image width="32" height="32" source="../images/gateSmall.png"/>
- </tile>
-</tileset>
--- DwarfGame/core/assets/maps/mobs.tsx
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="mobs" tilewidth="25" tileheight="32" tilecount="1" columns="0">
- <tile id="0">
-  <image width="25" height="32" source="../images/mobIcons/skeleton.png"/>
- </tile>
-</tileset>
